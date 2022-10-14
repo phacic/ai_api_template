43de7a5773bb783cea921d51cc028a98c43419b6
Karlmichael <phacic@msn.com>
2022-10-14 12:49:16 +0000

    improved test

--- {{cookiecutter.model_name}}/tests/test_main.py
+++ {{cookiecutter.model_name}}/tests/test_main.py
@@ -1,9 +1,10 @@
 import pytest
 from fastapi import status
+from fastapi.testclient import TestClient
 
 
 @pytest.mark.asyncio
-async def test_root(app_client):
+async def test_root(app_client: TestClient):
     resp = app_client.get("/")
 
     # should be redirect response with status 307
